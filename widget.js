50d426a76ea0042f7617de258c7989e0f1a745e7
urirolls5987 <urirolls@college.harvard.edu>
2024-08-18 12:52:46 +0100

    redo

--- widget.js
+++ widget.js
@@ -1,38 +1,41 @@
 (function() {
     var widgetUrl = 'https://urirolls5987.github.io/accessibros_widget/index.html';
+    var cssUrl = 'https://urirolls5987.github.io/accessibros_widget/style.css';
     
+    function loadCSS(url) {
+        var link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = url;
+        document.head.appendChild(link);
+    }
+
     function loadWidget() {
-      var iframe = document.createElement('iframe');
-      iframe.src = widgetUrl;
-      iframe.style.position = 'fixed';
-      iframe.style.bottom = '20px';
-      iframe.style.right = '20px';
-      iframe.style.width = '54px';
-      iframe.style.height = '54px';
-      iframe.style.border = 'none';
-      iframe.style.zIndex = '999999';
-      document.body.appendChild(iframe);
-  
-      window.addEventListener('message', function(event) {
-        if (event.origin !== new URL(widgetUrl).origin) return;
-        
-        if (event.data.type === 'toggleMenu') {
-          if (event.data.open) {
-            iframe.style.width = '500px';
-            iframe.style.height = 'calc(100% - 40px - 75px)';
-          } else {
-            iframe.style.width = '54px';
-            iframe.style.height = '54px';
-          }
-        } else if (event.data.type === 'applyStyles') {
-          document.body.style.cssText += event.data.styles;
-        }
-      });
+        var iframe = document.createElement('iframe');
+        iframe.src = widgetUrl;
+        iframe.style.position = 'fixed';
+        iframe.style.bottom = '20px';
+        iframe.style.right = '20px';
+        iframe.style.width = '320px';
+        iframe.style.height = '60px';
+        iframe.style.border = 'none';
+        iframe.style.zIndex = '999999';
+        document.body.appendChild(iframe);
+
+        window.addEventListener('message', function(event) {
+            if (event.origin !== new URL(widgetUrl).origin) return;
+            
+            if (event.data.type === 'toggleMenu') {
+                iframe.style.height = event.data.open ? '100%' : '60px';
+            } else if (event.data.type === 'applyStyles') {
+                document.body.style.cssText += event.data.styles;
+            }
+        });
     }
-  
+
+    loadCSS(cssUrl);
     if (document.readyState === 'complete') {
-      loadWidget();
+        loadWidget();
     } else {
-      window.addEventListener('load', loadWidget);
+        window.addEventListener('load', loadWidget);
     }
-  })();
+})();
