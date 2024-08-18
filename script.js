1def8291fbf70c30d3f126709d122737aea14f9a
urirolls5987 <urirolls@college.harvard.edu>
2024-08-18 12:55:58 +0100

    changed script

--- script.js
+++ script.js
@@ -19,30 +19,19 @@ window.AccessibilityWidget = {
 
   adjustFontSize: function (multiply = 0) {
     console.log("Adjusting font size");
-    const storedPercentage = parseFloat(localStorage.getItem("fontPercentage"));
-    if (multiply) {
-      if (storedPercentage) {
-        const newPercentage = storedPercentage + multiply;
-        localStorage.setItem("fontPercentage", newPercentage);
-      } else {
-        const newPercentage = 1 + multiply;
-        localStorage.setItem("fontPercentage", newPercentage);
-      }
-    }
-    const percentage = parseFloat(localStorage.getItem("fontPercentage")) || 1;
-    this.applyStyles(`* { font-size: ${percentage * 100}% !important; }`);
+    const storedPercentage =
+      parseFloat(localStorage.getItem("fontPercentage")) || 1;
+    const newPercentage = storedPercentage + multiply;
+    localStorage.setItem("fontPercentage", newPercentage);
+    this.applyStyles(`* { font-size: ${newPercentage * 100}% !important; }`);
   },
 
   adjustLetterSpacing: function (increment = 0) {
     console.log("Adjusting letter spacing");
-    let isLetterSpacingEnabled = parseInt(
-      localStorage.getItem("isLetterSpacingEnabled")
-    );
-    if (!increment) {
-      isLetterSpacingEnabled = !isLetterSpacingEnabled;
-      increment = 0.1;
-    }
-    if (!isLetterSpacingEnabled) {
+    let isLetterSpacingEnabled =
+      parseInt(localStorage.getItem("isLetterSpacingEnabled")) || 0;
+    isLetterSpacingEnabled = !isLetterSpacingEnabled;
+    if (isLetterSpacingEnabled) {
       this.applyStyles(`* { letter-spacing: ${increment}em !important; }`);
       localStorage.setItem("isLetterSpacingEnabled", 1);
     } else {
@@ -51,15 +40,12 @@ window.AccessibilityWidget = {
     }
   },
 
-  enableDyslexicFont: function (load = false) {
+  enableDyslexicFont: function () {
     console.log("Toggling dyslexic font");
-    let isDyslexicFontEnabled = parseInt(
-      localStorage.getItem("isDyslexicFontEnabled")
-    );
-    if (load) {
-      isDyslexicFontEnabled = !isDyslexicFontEnabled;
-    }
-    if (!isDyslexicFontEnabled) {
+    let isDyslexicFontEnabled =
+      parseInt(localStorage.getItem("isDyslexicFontEnabled")) || 0;
+    isDyslexicFontEnabled = !isDyslexicFontEnabled;
+    if (isDyslexicFontEnabled) {
       this.applyStyles(
         `* { font-family: OpenDyslexic3, sans-serif !important; }`
       );
@@ -70,15 +56,12 @@ window.AccessibilityWidget = {
     }
   },
 
-  enableBigCursor: function (load = false) {
+  enableBigCursor: function () {
     console.log("Toggling big cursor");
-    let isBigCursorEnabled = parseInt(
-      localStorage.getItem("isBigCursorEnabled")
-    );
-    if (load) {
-      isBigCursorEnabled = !isBigCursorEnabled;
-    }
-    if (!isBigCursorEnabled) {
+    let isBigCursorEnabled =
+      parseInt(localStorage.getItem("isBigCursorEnabled")) || 0;
+    isBigCursorEnabled = !isBigCursorEnabled;
+    if (isBigCursorEnabled) {
       this.applyStyles(
         `* { cursor: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='64' height='64' viewBox='0 0 512 512'%3E%3Cpath d='M429.742 319.31L82.49 0l-.231 471.744 105.375-100.826 61.89 141.083 96.559-42.358-61.89-141.083 145.549-9.25zM306.563 454.222l-41.62 18.259-67.066-152.879-85.589 81.894.164-333.193 245.264 225.529-118.219 7.512 67.066 152.878z' xmlns='http://www.w3.org/2000/svg'/%3E%3C/svg%3E"), default !important; }`
       );
@@ -89,13 +72,12 @@ window.AccessibilityWidget = {
     }
   },
 
-  enableHighlightLinks: function (load = false) {
+  enableHighlightLinks: function () {
     console.log("Toggling highlight links");
-    let isHighlightLinks = parseInt(localStorage.getItem("isHighlightLinks"));
-    if (load) {
-      isHighlightLinks = !isHighlightLinks;
-    }
-    if (!isHighlightLinks) {
+    let isHighlightLinks =
+      parseInt(localStorage.getItem("isHighlightLinks")) || 0;
+    isHighlightLinks = !isHighlightLinks;
+    if (isHighlightLinks) {
       this.applyStyles(
         `a, button { text-decoration: underline !important; font-weight: 800 !important; font-size: 110% !important; color: #ff0000 !important; }`
       );
@@ -108,15 +90,12 @@ window.AccessibilityWidget = {
     }
   },
 
-  enableHighlightHeadings: function (load = false) {
+  enableHighlightHeadings: function () {
     console.log("Toggling highlight headings");
-    let isHighlightHeadings = parseInt(
-      localStorage.getItem("isHighlightHeadings")
-    );
-    if (load) {
-      isHighlightHeadings = !isHighlightHeadings;
-    }
-    if (!isHighlightHeadings) {
+    let isHighlightHeadings =
+      parseInt(localStorage.getItem("isHighlightHeadings")) || 0;
+    isHighlightHeadings = !isHighlightHeadings;
+    if (isHighlightHeadings) {
       this.applyStyles(
         `h1, h2, h3, heading1 { color: #ff0000 !important; text-decoration: underline !important; }`
       );
@@ -129,17 +108,13 @@ window.AccessibilityWidget = {
     }
   },
 
-  adjustLineHeight: function (increment = 0) {
+  adjustLineHeight: function () {
     console.log("Adjusting line height");
-    let isLineHeightEnabled = parseInt(
-      localStorage.getItem("isLineHeightEnabled")
-    );
-    if (!increment) {
-      isLineHeightEnabled = !isLineHeightEnabled;
-      increment = 1;
-    }
-    if (!isLineHeightEnabled) {
-      this.applyStyles(`* { line-height: ${1.5 + increment} !important; }`);
+    let isLineHeightEnabled =
+      parseInt(localStorage.getItem("isLineHeightEnabled")) || 0;
+    isLineHeightEnabled = !isLineHeightEnabled;
+    if (isLineHeightEnabled) {
+      this.applyStyles(`* { line-height: 2 !important; }`);
       localStorage.setItem("isLineHeightEnabled", 1);
     } else {
       this.applyStyles(`* { line-height: normal !important; }`);
@@ -147,17 +122,13 @@ window.AccessibilityWidget = {
     }
   },
 
-  adjustFontWeight: function (increment = 100) {
+  adjustFontWeight: function () {
     console.log("Adjusting font weight");
-    let isFontWeightEnabled = parseInt(
-      localStorage.getItem("isFontWeightEnabled")
-    );
-    if (increment === 100) {
-      isFontWeightEnabled = !isFontWeightEnabled;
-      increment = 400;
-    }
-    if (!isFontWeightEnabled) {
-      this.applyStyles(`* { font-weight: ${400 + increment} !important; }`);
+    let isFontWeightEnabled =
+      parseInt(localStorage.getItem("isFontWeightEnabled")) || 0;
+    isFontWeightEnabled = !isFontWeightEnabled;
+    if (isFontWeightEnabled) {
+      this.applyStyles(`* { font-weight: bold !important; }`);
       localStorage.setItem("isFontWeightEnabled", 1);
     } else {
       this.applyStyles(`* { font-weight: normal !important; }`);
@@ -165,13 +136,12 @@ window.AccessibilityWidget = {
     }
   },
 
-  adjustContrast: function (load = false) {
+  adjustContrast: function () {
     console.log("Adjusting contrast");
-    let isContrastEnabled = parseInt(localStorage.getItem("isContrastEnabled"));
-    if (load) {
-      isContrastEnabled = !isContrastEnabled;
-    }
-    if (!isContrastEnabled) {
+    let isContrastEnabled =
+      parseInt(localStorage.getItem("isContrastEnabled")) || 0;
+    isContrastEnabled = !isContrastEnabled;
+    if (isContrastEnabled) {
       this.applyStyles(
         `* { color: #ffff00 !important; background-color: #0000ff !important; }`
       );
@@ -187,20 +157,9 @@ window.AccessibilityWidget = {
   reset: function () {
     console.log("Resetting all settings");
     localStorage.clear();
-    this.onPageLoad();
-  },
-
-  onPageLoad: function () {
-    console.log("Running onPageLoad function");
-    this.adjustFontSize();
-    this.adjustLetterSpacing();
-    this.enableDyslexicFont(true);
-    this.enableBigCursor(true);
-    this.enableHighlightLinks(true);
-    this.enableHighlightHeadings(true);
-    this.adjustLineHeight();
-    this.adjustFontWeight();
-    this.adjustContrast(true);
+    this.applyStyles(
+      `* { font-size: 100% !important; letter-spacing: normal !important; font-family: inherit !important; cursor: default !important; text-decoration: inherit !important; font-weight: inherit !important; color: inherit !important; background-color: inherit !important; line-height: normal !important; }`
+    );
   },
 
   attachEventListeners: function () {
@@ -222,33 +181,32 @@ window.AccessibilityWidget = {
       .addEventListener("click", () => this.adjustFontSize(-0.1));
     document
       .getElementById("adjustLineHeight")
-      .addEventListener("click", () => this.adjustLineHeight(1));
+      .addEventListener("click", this.adjustLineHeight.bind(this));
     document
       .getElementById("adjustLetterSpacing")
       .addEventListener("click", () => this.adjustLetterSpacing(0.1));
     document
       .getElementById("enableDyslexicFont")
-      .addEventListener("click", () => this.enableDyslexicFont());
+      .addEventListener("click", this.enableDyslexicFont.bind(this));
     document
       .getElementById("enableHighlightHeadings")
-      .addEventListener("click", () => this.enableHighlightHeadings());
+      .addEventListener("click", this.enableHighlightHeadings.bind(this));
     document
       .getElementById("enableHighlightLinks")
-      .addEventListener("click", () => this.enableHighlightLinks());
+      .addEventListener("click", this.enableHighlightLinks.bind(this));
     document
       .getElementById("adjustFontWeight")
-      .addEventListener("click", () => this.adjustFontWeight(400));
+      .addEventListener("click", this.adjustFontWeight.bind(this));
     document
       .getElementById("enableBigCursor")
-      .addEventListener("click", () => this.enableBigCursor());
+      .addEventListener("click", this.enableBigCursor.bind(this));
     document
       .getElementById("adjustContrast")
-      .addEventListener("click", () => this.adjustContrast());
+      .addEventListener("click", this.adjustContrast.bind(this));
   },
 
   init: function () {
     console.log("Initializing accessibility widget");
-    this.onPageLoad();
     this.attachEventListeners();
     // Notify the parent window that the widget is ready
     window.parent.postMessage({ type: "widgetReady" }, "*");
