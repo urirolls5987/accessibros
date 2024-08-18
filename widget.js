dbb6fcc8b7893dafae426146824280f7821148d5
urirolls5987 <urirolls@college.harvard.edu>
2024-08-18 11:24:21 +0100

    changed urls

--- widget.js
+++ widget.js
@@ -2,11 +2,11 @@
     // 1. Dynamically load external CSS file
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://yourdomain.com/index.css'; // Replace with your hosted CSS file URL
+    link.href = 'https://github.com/urirolls5987/accessibros_widget/blob/main/styles.css';
     document.head.appendChild(link);
   
     // 2. Fetch the HTML structure from the hosted HTML file
-    fetch('https://yourdomain.com/widget.html') // Replace with the URL to your hosted HTML file
+    fetch('https://github.com/urirolls5987/accessibros_widget/blob/main/index.html')
       .then(response => response.text())
       .then(data => {
         // 3. Inject the HTML into the body
